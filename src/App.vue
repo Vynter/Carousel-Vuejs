f30340bd12ade20a877030d84c56623b79959988
Vynter <amine-cheraitia@hotmail.com>
2020-11-08 22:04:29 +0100

    first

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <i class="fas fa-robot"></i>
+    <i class="fas fa-arrow-left"></i>
+    <i class="fas fa-arrow-right"></i>-->
+
+    <!-- <font-awesome-icon :icon="['fas', 'arrow-circle-left']" />
+    <font-awesome-icon :icon="['fas', 'arrow-circle-right']" /> -->
+    <Slider></Slider>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Slider from "./components/Slider.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Slider,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Slider.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div class="mainSlide">
+      <a class="left" href="">
+        <font-awesome-icon class="icon" :icon="['fas', 'arrow-circle-left']"
+      /></a>
+      <div class="slidecontainer">
+        <img src="../assets/1 800x600.jpg" alt="" />
+        <img src="../assets/2 800x600.jpg" alt="" />
+        <img src="../assets/3 800x600.jpg" alt="" />
+        <img src="../assets/4 800x600.jpg" alt="" />
+      </div>
+      <a class="right" href="">
+        <font-awesome-icon class="icon" :icon="['fas', 'arrow-circle-right']"
+      /></a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Slider",
+};
+</script>
+
+<style scoped>
+.mainSlide {
+  position: relative;
+
+  border: 1px solid red;
+}
+.slidecontainer {
+  width: 800px;
+  height: 600px;
+  margin: auto;
+  border: 1px solid blue;
+}
+.left,
+.right {
+  text-decoration: none;
+  display: inline-block;
+  position: absolute;
+  top: 50%;
+  color: black;
+}
+.left {
+  left: 10%;
+}
+.right {
+  right: 10%;
+}
+.icon {
+  font-size: 38px;
+}
+</style>
